f740b5451216febf6b5f55b54ec526f0e4d3a6ea
Jonathan Almeida <almeidajonathaan@gmail.com>
2022-08-29 17:47:16 -0300

    add condicionais e novas rotas

--- src/server.js
+++ src/server.js
@@ -5,6 +5,7 @@ const server = express()
 
 
 server.use(express.static("public"))
+server.use(express.urlencoded({ extended: true }))
 
 //utilizando template engine
 const nunjucks = require('nunjucks')
@@ -19,26 +20,63 @@ server.get("/", (req, res) => {
 })
 
 server.get("/point", (req, res) => {
+    // console.log(req.query)
     return res.render("create-point.html")
 })
 
+server.post("/savepoint", (req, res) => {
+    const query = `INSERT INTO places(name, image, address, address2, state, city, items) VALUES (?, ?, ?, ?, ?, ?, ?);`
+    // var { name, image, address, address2, uf, city, state, items } = body.req;
+    // const values = [name, image, address, address2, uf, city, state, items]
+    const values = [
+        req.body.name,
+        req.body.image,
+        req.body.address,
+        req.body.address2,
+        req.body.state,
+        req.body.city,
+        req.body.items
+    ]
+    function afterInsertData(err) {
+        let type = 'successful';
+        if (err) {
+            type = "error";
+            // return console.log(err)
+        } else {
+            console.log('Cadastro realizado!')
+            console.log(this)
+        }
+        console.log(`type: ${type}`)
+        return res.render("create-point.html", { type })
+    }
+
+    db.run(query, values, afterInsertData)
+})
+
 server.get("/search", (req, res) => {
+
+    const search = req.query.search
+    console.log(`search: ${search}`)
+    if (search == " ") {
+        return res.render("list-results.html", { total: 0 })
+    }
+
+
+        db.all(`SELECT * FROM places`, function (err, rows) {
+            if (err) {
+                return console.log(err)
+            } else {
+                console.log("Aqui estão seus registros")
+                console.log(rows)
+            }
+
+            const total = rows.length
+
+            //exibir a página com os dados do db
+            return res.render("list-results.html", { places: rows, total })
+
+        })
     
-    db.all(`SELECT * FROM places`, function(err, rows) {
-                if(err) {
-                    return console.log(err)
-                } else {
-                    console.log("Aqui estão seus registros")
-                    console.log(rows)
-                }
-                
-                const total = rows.length
-
-                //exibir a página com os dados do db
-                return res.render("list-results.html", { places: rows, total})
-
-            })
-
 })
 
 
